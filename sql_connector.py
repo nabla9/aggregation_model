56b8138c74aea9a455ed3dcd4db2ff42bb639bb1
Adam Wilander <awilande@css.edu>
2020-09-19 23:18:53 -0600

    Start record_data method for SQLConnector
    TODO: need to adjust definition of times and nodes to scale with data.

--- sql_connector.py
+++ sql_connector.py
@@ -25,3 +25,34 @@ def read_config(name, group):
 class SQLConnector:
     def __init__(self, *, cfgfile='agg.conf'):
         cfg = read_config(cfgfile, 'SQL')
+        self._connect = mysql.connect(**cfg)
+        self._cursor = self._connect.cursor()
+
+    def close(self):
+        self._cursor.close()
+        self._connect.close()
+
+    def fetchall(self):
+        self._cursor.fetchall()
+
+    def record_data(self, data):
+        try:
+            self.fetchall()
+        except mysql.errors.InterfaceError:
+            pass
+        self._cursor.execute('SELECT MAX(sim_id) FROM simulations')
+        (x,) = self.fetchall()[0]
+        sim_id = x if x else 1  # TODO: change back to x+1 here later
+
+        # TODO: temporary, change this later
+        sim_id = [sim_id]*1000
+        times = np.arange(1001)
+        nodes = np.arange(1,1001)
+        for idx, row in enumerate(data):
+            times_ar = [times[idx]]*1000
+            data = zip(sim_id, nodes, times_ar, row)
+            data_str = [str(tup) for tup in data]
+            commit_state = 'INSERT INTO simdata VALUES ' + ', '.join(data_str)
+            self._cursor.execute(commit_state)
+            self._connect.commit()
+
