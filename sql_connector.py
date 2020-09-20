444e95ceb3fd83bcf0e5e69aab7a8fe4fe8e7b15
Adam Wilander <awilande@css.edu>
2020-09-20 11:57:11 -0600

    Finalize function for sim insertion in sql_connector.py
    We still need to connect this to plottools.SolutionWrapper, perhaps as a nested Class.

--- sql_connector.py
+++ sql_connector.py
@@ -35,24 +35,37 @@ class SQLConnector:
     def fetchall(self):
         self._cursor.fetchall()
 
-    def record_data(self, data):
+    def record_data(self, params, data):
         try:
             self.fetchall()
         except mysql.errors.InterfaceError:
             pass
         self._cursor.execute('SELECT MAX(sim_id) FROM simulations')
         (x,) = self.fetchall()[0]
-        sim_id = x if x else 1  # TODO: change back to x+1 here later
 
-        # TODO: temporary, change this later
-        sim_id = [sim_id]*1000
-        times = np.arange(1001)
-        nodes = np.arange(1,1001)
+        # Insert record into simulations table
+        sim_id = x + 1 if x else 1
+        graph = params['graph']
+        graph_str = str(graph).replace('\n ', ',')
+        n_nodes = graph.shape[0]
+        n_comms = len(graph.comms)
+        ker_a = params['a']
+        ker_b = params['b']
+        insert = ("INSERT INTO simulations (sim_id,graph,n_nodes,n_comms,ker_a,ker_b) "
+                  "VALUES (%s, '%s', %s, %s, %s, %s)"
+                  % (sim_id, graph_str, n_nodes, n_comms, ker_a, ker_b))
+        self._cursor.execute(insert)
+        self._connect.commit()
+
+        # Insert data into simdata table
+        sim_id = [sim_id]*n_nodes
+        times = params['times']
+        nodes = np.arange(1, n_nodes+1)
         for idx, row in enumerate(data):
             times_ar = [times[idx]]*1000
             data = zip(sim_id, nodes, times_ar, row)
             data_str = [str(tup) for tup in data]
             commit_state = 'INSERT INTO simdata VALUES ' + ', '.join(data_str)
             self._cursor.execute(commit_state)
-            self._connect.commit()
+        self._connect.commit()  # Do I have to commit after each loop?
 
