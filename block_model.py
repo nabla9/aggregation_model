25bc1285482627cd89fded6e72b2902b67de81c4
Adam Wilander <awilande@css.edu>
2020-09-21 23:24:59 -0600

    Clean up import statement in block_model.py

--- block_model.py
+++ block_model.py
@@ -1,5 +1,4 @@
-from numpy.random import choice
-from numpy import ndarray, cumsum, zeros, triu
+import numpy as np
 
 
 def create_block_model(comm_list, prob_array):
@@ -7,9 +6,9 @@ def create_block_model(comm_list, prob_array):
     A simple function to create a stochastic block model adjacency matrix.
 
     :param list comm_list: A 1D list of individuals assigned to respective communities.
-    :param ndarray prob_array: A 2D array of interconnection probabilities. P[i,j] is the probability
+    :param np.ndarray prob_array: A 2D array of interconnection probabilities. P[i,j] is the probability
                               of connection between individuals from comm i,j.
-    :return ndarray adj: A symmetric adjacency matrix of 0's and 1's corresponding to edges between individuals.
+    :return np.ndarray adj: A symmetric adjacency matrix of 0's and 1's corresponding to edges between individuals.
 
     Implementation
     --------------
@@ -22,9 +21,9 @@ def create_block_model(comm_list, prob_array):
     members can differ significantly from the density of links with non-members.
 
     """
-    comm_idx = cumsum(comm_list)
+    comm_idx = np.cumsum(comm_list)
     n_nodes = comm_idx[-1]
-    adj = zeros([n_nodes, n_nodes])
+    adj = np.zeros([n_nodes, n_nodes])
 
     for r_idx, r in enumerate(comm_idx):
         for c_idx, c in enumerate(comm_idx):
@@ -34,10 +33,10 @@ def create_block_model(comm_list, prob_array):
             else:
                 rl = comm_idx[r_idx - 1] if r_idx != 0 else 0
                 cl = comm_idx[c_idx - 1] if c_idx != 0 else 0
-                adj[rl:r, cl:c] = choice([0, 1], [r - rl, c - cl], p=[1 - p_conn, p_conn])
+                adj[rl:r, cl:c] = np.random.choice([0, 1], [r - rl, c - cl], p=[1 - p_conn, p_conn])
 
     # Zero sampled entries below/including the main diagonal and add transpose to fill in the rest
-    adj = triu(adj, 1)
+    adj = np.triu(adj, 1)
     adj += adj.transpose()
     return adj
 
